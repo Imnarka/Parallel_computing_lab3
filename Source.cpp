4ab8d836d8c2c69c76720ee1d3ee4cd95705f2aa
lmnarka <lnarka09@gmail.com>
2022-02-25 15:51:34 +0500

    Wrote code for parallel matrix multiplication

--- Source.cpp
+++ Source.cpp
@@ -7,8 +7,9 @@
 #include <windows.h>
 
 using std::cout;
+using std::cin;
 
-void problem1(int N) {
+void problem_1(int N) {
 	srand(time(NULL));
 	double** M1 = new double* [N];
 	for (int i = 0; i < N; i++) {
@@ -55,17 +56,109 @@ void problem1(int N) {
 		for (int j = 0; j < N; j++) {
 			M3[i][j] = 0.0;
 			for (int k = 0; k < N; k++) {
-				M3[i][j] += M1[i][k] * M2[k][j];
-				cout << M3[i][j] << " ";
+				M3[i][j] += (M1[i][k] * M2[k][j]);
 			}
-			cout << "\n";
+			cout << M3[i][j] << "\t";
+		}
+		cout << "\n";
+	}
+	for (int i = 0; i < N; i++)
+	{
+		delete[]M1[i];
+		delete[]E[i];
+		delete[]M2[i];
+		delete[]M3[i];
+	}
+	delete[]M1;
+	delete[]E;
+	delete[]M2;
+	delete[]M3;
+}
+
+void problem_2(int N, int num_threads) {
+	srand(time(NULL));
+	double** M1 = new double* [N];
+	for (int i = 0; i < N; i++) {
+		M1[i] = new double[N];
+	}
+	printf("Matrix M1\n");
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			M1[i][j] = (double)rand() / RAND_MAX * (12 - 10) - 10;
+			cout << M1[i][j] << " ";
+		}
+		cout << "\n";
+	}
+	double** E = new double* [N];
+	for (int i = 0; i < N; i++) {
+		E[i] = new double[N];
+	}
+	printf("Matrix E\n");
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			E[i][j] = (double)rand() / RAND_MAX * (10 - 0) + 0;
+			cout << E[i][j] << " ";
+		}
+		cout << "\n";
+	}
+	double** M2 = new double* [N];
+	for (int i = 0; i < N; i++) {
+		M2[i] = new double[N];
+	}
+	printf("Matrix M2\n");
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			M2[i][j] = M1[i][j] - E[i][j];
+			cout << M2[i][j] << " ";
+		}
+		cout << "\n";
+	}
+	double** M3 = new double* [N];
+	for (int i = 0; i < N; i++) {
+		M3[i] = new double[N];
+	}
+	omp_set_num_threads(num_threads);
+	int i, j, k;
+	printf("Matrix M3\n");
+#pragma omp parallel for shared(M1, M2, M3) private(i,j,k)
+	for (i = 0; i < N; i++) {
+		for (j = 0; j < N; j++) {
+			M3[i][j] = 0.0;
+			for (k = 0; k < N; k++) {
+				M3[i][j] += (M1[i][k] * M2[k][j]);
+
+			}
 		}
 	}
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			printf("%f\t", M3[i][j]);
+		}
+		printf("\n");
+	}
+
+	for (int i = 0; i < N; i++)
+	{
+		delete[]M1[i];
+		delete[]E[i];
+		delete[]M2[i];
+		delete[]M3[i];
+	}
+	delete[]M1;
+	delete[]E;
+	delete[]M2;
+	delete[]M3;
 }
 
 int main()
 {
 	int N;
-	scanf("%d", N);
-	problem1(N);
+	int num_threads;
+	/*scanf("%d", N);
+	scanf("%d", num_threads);*/
+	cin >> N;
+	cin >> num_threads;
+	problem_1(N);
+	cout << "Parallel solution\n";
+	problem_2(N, num_threads);
 }
