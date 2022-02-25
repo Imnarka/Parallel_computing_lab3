b92b4cd5a93b2b192c6edd6aacdc402946b8355b
lmnarka <lnarka09@gmail.com>
2022-02-25 22:58:25 +0500

    Added time calculation, acceleration and efficiency

--- Source.cpp
+++ Source.cpp
@@ -5,11 +5,13 @@
 #include <locale.h>
 #include <iostream>
 #include <windows.h>
+#include <ctime>
+#include <time.h>
 
 using std::cout;
 using std::cin;
 
-void problem_1(int N) {
+double problem_1(int N) {
 	srand(time(NULL));
 	double** M1 = new double* [N];
 	for (int i = 0; i < N; i++) {
@@ -51,7 +53,11 @@ void problem_1(int N) {
 	for (int i = 0; i < N; i++) {
 		M3[i] = new double[N];
 	}
+	//double start = clock();
+
 	printf("Matrix M3\n");
+	clock_t start, end;
+	start = clock();
 	for (int i = 0; i < N; i++) {
 		for (int j = 0; j < N; j++) {
 			M3[i][j] = 0.0;
@@ -62,6 +68,9 @@ void problem_1(int N) {
 		}
 		cout << "\n";
 	}
+	end = clock();
+	printf("Время %lf \n", ((double)end - start) / ((double)CLOCKS_PER_SEC));
+
 	for (int i = 0; i < N; i++)
 	{
 		delete[]M1[i];
@@ -73,9 +82,10 @@ void problem_1(int N) {
 	delete[]E;
 	delete[]M2;
 	delete[]M3;
+	return ((double)end - start) / ((double)CLOCKS_PER_SEC);
 }
 
-void problem_2(int N, int num_threads) {
+double problem_2(int N, int num_threads) {
 	srand(time(NULL));
 	double** M1 = new double* [N];
 	for (int i = 0; i < N; i++) {
@@ -120,6 +130,7 @@ void problem_2(int N, int num_threads) {
 	omp_set_num_threads(num_threads);
 	int i, j, k;
 	printf("Matrix M3\n");
+	double start_time = omp_get_wtime();
 #pragma omp parallel for shared(M1, M2, M3) private(i,j,k)
 	for (i = 0; i < N; i++) {
 		for (j = 0; j < N; j++) {
@@ -130,13 +141,16 @@ void problem_2(int N, int num_threads) {
 			}
 		}
 	}
+	double end_time = omp_get_wtime();
 	for (int i = 0; i < N; i++) {
 		for (int j = 0; j < N; j++) {
 			printf("%f\t", M3[i][j]);
 		}
 		printf("\n");
 	}
-
+	cout << "----------------------------\n";
+	printf("Время %lf \n", end_time - start_time);
+	//printf("Точность таймера %lf ", omp_get_wtick());
 	for (int i = 0; i < N; i++)
 	{
 		delete[]M1[i];
@@ -148,17 +162,26 @@ void problem_2(int N, int num_threads) {
 	delete[]E;
 	delete[]M2;
 	delete[]M3;
+	return end_time - start_time;
 }
 
 int main()
 {
+	setlocale(LC_ALL, "RU");
 	int N;
 	int num_threads;
 	/*scanf("%d", N);
 	scanf("%d", num_threads);*/
 	cin >> N;
 	cin >> num_threads;
-	problem_1(N);
+
+	double time1 = problem_1(N);
+
 	cout << "Parallel solution\n";
-	problem_2(N, num_threads);
+	double time2 = problem_2(N, num_threads);
+
+	printf("----------Расчёт ускорения и эффетиквности----------\n");
+	float boost = time1 / time2;
+	printf("Ускорение: %f\n", boost);
+	printf("Эффективность: %f", boost/num_threads);
 }
